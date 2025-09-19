9a164df4d237ffc093c9917a7901eedfd3f5df7d
Nicholas Ocket <nicholas@aboutcoding.be>
2025-09-19 12:50:30 +0200

    deleted all but exercise one person - car exercise

--- src/main/java/be/aboutcoding/jpadojo/entityrelations/OneToOneController.java
+++ src/main/java/be/aboutcoding/jpadojo/entityrelations/OneToOneController.java
@@ -1,12 +1,7 @@
 package be.aboutcoding.jpadojo.entityrelations;
 
 
-import be.aboutcoding.jpadojo.entityrelations.onetoone.domain.employeebadge.Badge;
-import be.aboutcoding.jpadojo.entityrelations.onetoone.domain.employeebadge.Employee;
 import be.aboutcoding.jpadojo.entityrelations.onetoone.domain.personcar.Person;
-import be.aboutcoding.jpadojo.entityrelations.onetoone.dto.BadgeResponse;
-import be.aboutcoding.jpadojo.entityrelations.onetoone.dto.CreateEmployeeRequest;
-import be.aboutcoding.jpadojo.entityrelations.onetoone.dto.EmployeeResponse;
 import be.aboutcoding.jpadojo.entityrelations.onetoone.repository.BadgeRepository;
 import be.aboutcoding.jpadojo.entityrelations.onetoone.repository.EmployeeRepository;
 import be.aboutcoding.jpadojo.entityrelations.onetoone.repository.PersonRepository;
@@ -23,12 +18,6 @@ public class OneToOneController {
     @Autowired
     private PersonRepository personRepository;
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
-
-    @Autowired
-    private BadgeRepository badgeRepository;
-
 
     @GetMapping("/person")
     public ResponseEntity<List<Person>> getAllPersons() {
@@ -40,42 +29,5 @@ public class OneToOneController {
     public ResponseEntity<Person> createOrUpdatePerson(@RequestBody Person person) {
         Person savedPerson = personRepository.save(person);
         return ResponseEntity.ok(savedPerson);
-    }
-
-    @GetMapping("/employee")
-    public ResponseEntity<List<EmployeeResponse>> getAllEmployees() {
-        List<Employee> employees = employeeRepository.findAll();
-        List<EmployeeResponse> response = employees.stream()
-                .map(EmployeeResponse::from)
-                .toList();
-        return ResponseEntity.ok(response);
-    }
-
-    @PutMapping("/employee")
-    public ResponseEntity<EmployeeResponse> createOrUpdateEmployee(@RequestBody CreateEmployeeRequest request) {
-        // Convert DTO to entity
-        Employee employee = convertToEntity(request);
-
-        // Save entity
-        Employee savedEmployee = employeeRepository.save(employee);
-
-        // Convert back to response DTO
-        EmployeeResponse response = EmployeeResponse.from(savedEmployee);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/badge")
-    public ResponseEntity<List<BadgeResponse>> getAllBadges() {
-        List<Badge> badges = badgeRepository.findAll();
-        List<BadgeResponse> response = badges.stream()
-                .map(BadgeResponse::from)
-                .toList();
-        return ResponseEntity.ok(response);
-    }
-
-    private Employee convertToEntity(CreateEmployeeRequest request) {
-        Employee employee = new Employee();
-        // TODO: fill this in
-        return employee;
     }
 }
